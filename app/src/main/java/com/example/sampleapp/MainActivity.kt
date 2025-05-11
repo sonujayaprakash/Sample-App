1bc3bea438d33e3da19a5fe5cc57ccd71f8da6ed
sonujayaprakash <sonuprakash173@gmail.com>
2025-05-11 18:07:13 +0530

    1st day

--- app/src/main/java/com/example/sampleapp/MainActivity.kt
+++ app/src/main/java/com/example/sampleapp/MainActivity.kt
@@ -4,12 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.sampleapp.ui.theme.SampleAppTheme
 
@@ -18,7 +27,24 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
+            Scaffold(
+                modifier = Modifier
+                    .fillMaxSize()
+            )
+            { paddingValues ->
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(paddingValues)
+                        .background(Color.Green),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text(text = "Hello")
+                    Text(text = "World")
+                    Button(onClick = ) { }
+                }
+            }
         }
     }
 }
